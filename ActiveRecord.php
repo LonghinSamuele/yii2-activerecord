b6f322634d8dae57f610d44c2b74aa0493dcafc1
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2024-07-16 11:32:00 +0200

    Adding formName attribute to allow multiple filter in same page

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -14,53 +14,60 @@ use yii\web\NotFoundHttpException;
  */
 abstract class ActiveRecord extends \yii\db\ActiveRecord implements GridInterface
 {
-	use ModelSerializable;
-	use UrlTrait;
+    use ModelSerializable;
+    use UrlTrait;
 
-	/**
-	 * @return ActiveQuery
-	 */
-	public static function find()
-	{
-		return new ActiveQuery(static::class);
-	}
+    public $formName = '';
 
-	/**
-	 * @param string $class
-	 * @param array $link
-	 * @return ActiveQuery
-	 */
-	public function hasMany($class, $link)
-	{
-		return parent::hasMany($class, $link);
-	}
+    /**
+     * @return ActiveQuery
+     */
+    public static function find()
+    {
+        return new ActiveQuery(static::class);
+    }
 
-	/**
-	 * @param string $class
-	 * @param array $link
-	 */
-	public function hasOne($class, $link)
-	{
-		return parent::hasOne($class, $link);
-	}
+    /**
+     * @param string $class
+     * @param array $link
+     * @return ActiveQuery
+     */
+    public function hasMany($class, $link)
+    {
+        return parent::hasMany($class, $link);
+    }
 
-	public static function queryFrom()
-	{
-		return null;
-	}
+    /**
+     * @param string $class
+     * @param array $link
+     */
+    public function hasOne($class, $link)
+    {
+        return parent::hasOne($class, $link);
+    }
 
-	/**
-	 * Finds the static model based on its primary key value (or condition).
-	 * If the model is not found, a 404 HTTP exception will be thrown.
-	 * @return static
-	 * @throws NotFoundHttpException
-	 */
-	public static function getOne($condition)
-	{
-		if (($model = static::findOne($condition)) !== null) {
-			return $model;
-		}
+    public static function queryFrom()
+    {
+        return null;
+    }
 
-		throw new NotFoundHttpException(Yii::t('samuelelonghin/active-record', 'The requested model does not exist.'));
-	}
+    /**
+     * Finds the static model based on its primary key value (or condition).
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @return static
+     * @throws NotFoundHttpException
+     */
+    public static function getOne($condition)
+    {
+        if (($model = static::findOne($condition)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('samuelelonghin/active-record', 'The requested model does not exist.'));
+    }
+
+    public function formName()
+    {
+        return $this->formName ?: parent::formName();
+    }
 }
